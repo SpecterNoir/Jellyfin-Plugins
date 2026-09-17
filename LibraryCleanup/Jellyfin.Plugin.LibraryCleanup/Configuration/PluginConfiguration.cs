cce5838d3ae1f5f6b82ec95d616e2c43baab7009
SpecterNoir <leonardorivera04@gmail.com>
2026-09-17 03:02:08 -0400

    Add Library Cleanup configuration

---
+++ LibraryCleanup/Jellyfin.Plugin.LibraryCleanup/Configuration/PluginConfiguration.cs
@@ -0,0 +1,10 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.LibraryCleanup.Configuration;
+
+/// <summary>
+/// Library Cleanup configuration.
+/// </summary>
+public sealed class PluginConfiguration : BasePluginConfiguration
+{
+}
